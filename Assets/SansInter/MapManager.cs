e17a6d31ac76b8ac7fc8a8455e133c5317c6bf62
beubeuh <72401464+antoinebiogeau@users.noreply.github.com>
2024-01-23 22:44:50 +0100

    tt

--- Assets/SansInter/MapManager.cs
+++ Assets/SansInter/MapManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapManager : MonoBehaviour
+public class Mapmanager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,5 +14,9 @@ public class MapManager : MonoBehaviour
     void Update()
     {
         
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        other.GetComponent<SNC>().Die();
     }
 }
